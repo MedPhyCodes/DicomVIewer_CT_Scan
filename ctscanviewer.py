35e7c8ee192d140cd37c165ccf6fe4b7d2222249
Praveen R <praveenr.bmail@gmail.com>
2025-12-26 01:24:27 +0530

    Update ctscanviewer.py

--- ctscanviewer.py
+++ ctscanviewer.py
@@ -8,9 +8,6 @@ import os
 import io
 
 
-
-
-
 @st.cache_data
 def load_dicom(uploaded_file): # Extract ZIP into a temp folder 
     with zipfile.ZipFile(uploaded_file, "r") as zip_ref: zip_ref.extractall("uploaded_folder") # Read DICOM series 
@@ -29,27 +26,17 @@ def HU_mask(ct,hu_l,hu_h):
 uploaded_file = st.file_uploader("Upload the CT Scan ZIP folder", type=["zip"]) 
 
 if uploaded_file is not None:
-    imarray = load_dicom(uploaded_file)
-
-
-
-
-    
-    
-    
+    imarray = load_dicom(uploaded_file)  
     slice_number = st.slider("Slice Number: ",1,imarray.shape[0]-1,1)
     hu_low =st.slider("Lower HU value: ",imarray.min(),imarray.max(),1)
     hu_high =st.slider("Higher HU value: ",imarray.min(),imarray.max(),1)
-    
-
  
-    
     fig=px.imshow(HU_mask(imarray[slice_number],hu_low,hu_high),color_continuous_scale="gray")
     fig.update_layout( xaxis=dict(showticklabels=False), yaxis=dict(showticklabels=False),
     coloraxis_showscale=False,
     width=1000, height=800
-    
     )  
+    
     st.plotly_chart(fig, use_container_width=True)
 
 
