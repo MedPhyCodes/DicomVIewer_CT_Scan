97a39476e38d27081fb4c7a5cfa5d0d2fbc816a3
Praveen R <praveenr.bmail@gmail.com>
2025-12-26 00:51:55 +0530

    Update ctscanviewer.py

--- ctscanviewer.py
+++ ctscanviewer.py
@@ -37,35 +37,35 @@ def HU_mask(ct,hu_l,hu_h):
     return ct_l
 
 
-
-
-
-slice_number = st.slider("Slice Number: ",1,imarray.shape[0]-1,1)
-hu_low =st.slider("Lower HU value: ",imarray.min(),imarray.max(),1)
-hu_high =st.slider("Higher HU value: ",imarray.min(),imarray.max(),1)
-
-
-#add a drop down for window level presets
-#add a ruler tool to measure the distance between points
-#dropdown to switch to axial/sagital/coronal view
-#tool to measure distance
-
-
-
-
-
-
-
-
-fig=px.imshow(HU_mask(imarray[slice_number],hu_low,hu_high),color_continuous_scale="gray")
-fig.update_layout( xaxis=dict(showticklabels=False), yaxis=dict(showticklabels=False),
-coloraxis_showscale=False,
-width=1000, height=800
-
-)  
-st.plotly_chart(fig, use_container_width=True)
-
-
+    
+    
+    
+    slice_number = st.slider("Slice Number: ",1,imarray.shape[0]-1,1)
+    hu_low =st.slider("Lower HU value: ",imarray.min(),imarray.max(),1)
+    hu_high =st.slider("Higher HU value: ",imarray.min(),imarray.max(),1)
+    
+    
+    #add a drop down for window level presets
+    #add a ruler tool to measure the distance between points
+    #dropdown to switch to axial/sagital/coronal view
+    #tool to measure distance
+    
+    
+    
+    
+    
+    
+    
+    
+    fig=px.imshow(HU_mask(imarray[slice_number],hu_low,hu_high),color_continuous_scale="gray")
+    fig.update_layout( xaxis=dict(showticklabels=False), yaxis=dict(showticklabels=False),
+    coloraxis_showscale=False,
+    width=1000, height=800
+    
+    )  
+    st.plotly_chart(fig, use_container_width=True)
+    
+    
 
 
 
